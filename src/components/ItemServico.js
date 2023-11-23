cdff61ffb283e16d9d3a4f2e19db5beee1ced466
Cauacomtilnoa <126720628+Cauacomtilnoa@users.noreply.github.com>
2023-11-23 16:33:18 -0300

    Update ItemServico.js

--- src/components/ItemServico.js
+++ src/components/ItemServico.js
@@ -6,9 +6,9 @@ function ItemServico(props) {
             <div className="card" style={{width: "18rem"}}>
             <img src={props.img} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                
                 <p className="card-text">{props.desc}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                
             </div>
             </div>
         </div>
